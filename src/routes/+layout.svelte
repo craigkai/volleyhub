0c4ccc9950e6264ef38b38a37b88432458f094da
Craig Kaiser <craig@kaiserengineering.io>
2023-05-01 22:18:50 -0400

    add anlytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,11 @@
 	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
 
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+
 	export let data: LayoutData;
 
 	$: ({ supabase, session } = data);
