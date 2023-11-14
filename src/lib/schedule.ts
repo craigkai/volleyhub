397c6d1da1fcab127f0e170a191e91ab36bb78aa
Craig Kaiser <craig@kaiserengineering.io>
2023-11-14 00:59:04 -0500

    need to actually check for duplicate games

--- src/lib/schedule.ts
+++ src/lib/schedule.ts
@@ -15,6 +15,11 @@ class TeamsQueue {
 
     constructor(teams: string[]) {
         this.teams = teams;
+
+        if (this.teams.length % 2) {
+            this.teams.push("bye")
+        }
+
         this.playedLast = [];
         this.teamsAvailable = [];
     }
@@ -23,27 +28,39 @@ class TeamsQueue {
     Make all teams available for scheduling again
     */
     newRound(): void {
-        this.teamsAvailable = [...this.teams];
+        // We move each element over one so that the neighbors are new
+        let temp = [...this.teams];
+        console.log(temp)
+
+        temp.push(temp.shift());
+        console.log(temp)
+
+        this.teams = [...temp];
+        this.playedLast = [];
+        this.teamsAvailable = [...temp];
     }
 
     /*
     If we have at least two available teams then return true
     */
     hasNext(): boolean {
-        return this.teamsAvailable ? this.teamsAvailable.length > 2 : false;
+        return this.teamsAvailable ? this.teamsAvailable.length >= 2 : false;
     }
 
     findMatch(): [string, string] {
         if (!this.teamsAvailable || this.teamsAvailable?.length < 2) {
             console.error(`Found the following available teams: ${this.teamsAvailable}`);
-            throw "Not enough teams!";
+            throw 'Not enough teams!';
         }
 
         // Get a list of the teams that didn't play last round
-        let teamsWhoNeedToPlay = this.teamsAvailable?.filter((team: string) => !this.playedLast.includes(team));
+        let teamsWhoNeedToPlay = this.teamsAvailable?.filter(
+            (team: string) => !this.playedLast.includes(team)
+        );
+
         if (teamsWhoNeedToPlay.length < 2) {
             console.error(`Not enough teams left in this round, bye?: ${teamsWhoNeedToPlay}`);
-            throw "Not enough teams who have not played!!";
+            throw 'Not enough teams who have not played!!';
         }
 
         const [home, guest] = [teamsWhoNeedToPlay.pop(), teamsWhoNeedToPlay.pop()];
@@ -57,16 +74,17 @@ class TeamsQueue {
         this.teamsAvailable.splice(this.teamsAvailable.indexOf(home), 1);
         this.teamsAvailable.splice(this.teamsAvailable.indexOf(guest), 1);
 
-        this.playedLast.shift();
-        this.playedLast.shift();
-
+        if (this.playedLast.length >= this.teams.length) {
+            this.playedLast.shift();
+            this.playedLast.shift();
+        }
         return [home, guest];
     }
 }
 
 export function createSchedule(teams: string[], courts: string[], poolPlayGames: number): any {
     let schedule: any = {};
-    courts.forEach((court: string) => schedule[court] = []);
+    courts.forEach((court: string) => (schedule[court] = []));
 
     let teamsQueue = new TeamsQueue(teams);
 
@@ -77,15 +95,15 @@ export function createSchedule(teams: string[], courts: string[], poolPlayGames:
         // Each game we look at how many courts we have:
         while (teamsQueue.hasNext()) {
             // We are going to loop over each court for each `round`, we can cover multiple `rounds` until
-            // we run out of teams and then everyone has played one round of pool play and we can restart.
+            // we run out of teams and then everyone has played one game of pool play and we can restart.
             courts.forEach((court: string) => {
-                // We should check our last team in the array that we are going to `pop()` and confirm
-                // that the teams we are grabbing haven't already played one another
                 if (!teamsQueue.hasNext()) {
                     return;
                 }
+                let matchup = teamsQueue.findMatch();
+                console.debug(`Round ${i} court ${court} matchup found ${matchup}`);
 
-                schedule[court].push(teamsQueue.findMatch());
+                schedule[court].push(matchup);
             });
         }
     }
@@ -99,44 +117,64 @@ export function createSchedule(teams: string[], courts: string[], poolPlayGames:
 
 if (import.meta.vitest) {
     interface gamesPlayedMap {
-        [key: string]: number;
+        [key: string]: string[];
     }
 
     const { it, expect } = import.meta.vitest;
     // We should loop over even/odd number of courts, teams and divisions and
     // confirm that teams aren't sitting too often, facing the same team too much
     // etc
-    let teams: string[] = [];
-    let i: number = 0;
-    for (; i < 28;) {
-        teams.push(`team${i}`);
-        i++;
-    }
-    let courts = ['court1', 'court2', 'court3'];
-    let poolPlayGames = 2;
+    let inputs: any = [
+        {
+            teamsCount: 4,
+            courts: 2,
+            poolPlayGames: 3
+        }
+    ];
 
-    let schedule = createSchedule(teams, courts, poolPlayGames);
+    inputs.forEach((input: any) => {
+        let teams: string[] = [];
+        let i: number = 0;
+        for (; i < input.teamsCount; i++) {
+            teams.push(`team${i}`);
+        }
 
-    it('No teams sit for more than two games', () => {
-        // expect(schedule).toEqual({});
-    });
+        let courts = [];
+        for (i = 0; i < input.courts; i++) {
+            courts.push(`court${i}`);
+        }
 
-    it('Teams do not face the same team too much', () => {
-        // expect(schedule).toEqual({});
-    });
+        let schedule = createSchedule(teams, courts, input.poolPlayGames);
 
-    it('All teams play the correct number of games', () => {
         let gamesPlayedPerTeam: gamesPlayedMap = {};
         Object.keys(schedule).forEach((court: string) => {
             schedule[court].forEach((game: string) => {
                 const [home, guest] = game;
-                gamesPlayedPerTeam[guest] = gamesPlayedPerTeam[guest] + 1 || 1;
-                gamesPlayedPerTeam[home] = gamesPlayedPerTeam[home] + 1 || 1;
+                gamesPlayedPerTeam[guest] ? gamesPlayedPerTeam[guest].push(home) : gamesPlayedPerTeam[guest] = [home];
+                gamesPlayedPerTeam[home] ? gamesPlayedPerTeam[home].push(guest) : gamesPlayedPerTeam[home] = [guest];
             });
         });
 
-        Object.keys(gamesPlayedPerTeam).forEach((teamName: string) => {
-            expect(gamesPlayedPerTeam[teamName]).to.equal(poolPlayGames);
+        // TODO
+        it('No teams sit for more than two games', () => {
+            // expect(schedule).toEqual({});
+        });
+
+        it('Teams do not face the same team too much', () => {
+            Object.keys(gamesPlayedPerTeam).forEach((teamName: string) => {
+                let teamsSeen: any = {};
+                gamesPlayedPerTeam[teamName].forEach((team: string) => {
+                    console.log(input)
+                    expect(teamsSeen[team]).toEqual(undefined);
+                    teamsSeen[team] = 1;
+                });
+            });
+        });
+
+        it('All teams play the correct number of games', () => {
+            Object.keys(gamesPlayedPerTeam).forEach((teamName: string) => {
+                expect(gamesPlayedPerTeam[teamName].length).to.equal(input.poolPlayGames);
+            });
         });
     });
 }
