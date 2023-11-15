ba5adde0138352dc9640fc4af9745aae05ed3540
Craig Kaiser <craig@kaiserengineering.io>
2023-11-14 23:55:23 -0500

    add dummy web ui for schedule generation

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { create_schedule } from '$lib/schedule';
+
+	let schedule: any;
+	let teams: any, courts: any, pools: any;
+
+	function setSchedule() {
+		teams = teams.split(',');
+		schedule = create_schedule({ teams, courts, pools });
+	}
+</script>
+
+Teams:
+<input class="bg-gray-200 p-2 rounded" type="text" bind:value={teams} />
+
+Numbr of Courts:
+<input class="bg-gray-200 p-2 rounded" type="text" bind:value={courts} />
+
+Number of Pool Play Games:
+<input class="bg-gray-200 p-2 rounded" type="text" bind:value={pools} />
+
+<button class="rounded bg-gray-400 p-4 hover:bg-gray-600" on:click={() => setSchedule()}
+	>Generate Schedule</button
+>
+
+{JSON.stringify(schedule)}
