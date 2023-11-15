a1e5539b35fd996184771846416df2c4bb18858f
Craig Kaiser <craig@kaiserengineering.io>
2023-11-15 00:00:44 -0500

    styling

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -10,17 +10,26 @@
 	}
 </script>
 
-Teams:
-<input class="bg-gray-200 p-2 rounded" type="text" bind:value={teams} />
+<div class="flex flex-col place-content-center place-items-center place-self-center">
+	<div class="w-1/2 m-2">
+		Teams:
+		<input class="bg-gray-200 p-2 rounded" type="text" bind:value={teams} />
+	</div>
+	<div class="w-1/2 m-2">
+		Numbr of Courts:
+		<input class="bg-gray-200 p-2 rounded" type="text" bind:value={courts} />
+	</div>
 
-Numbr of Courts:
-<input class="bg-gray-200 p-2 rounded" type="text" bind:value={courts} />
-
-Number of Pool Play Games:
-<input class="bg-gray-200 p-2 rounded" type="text" bind:value={pools} />
-
-<button class="rounded bg-gray-400 p-4 hover:bg-gray-600" on:click={() => setSchedule()}
-	>Generate Schedule</button
->
-
-{JSON.stringify(schedule)}
+	<div class="w-1/2 m-2">
+		Number of Pool Play Games:
+		<input class="bg-gray-200 p-2 rounded" type="text" bind:value={pools} />
+	</div>
+	<div class="w-1/2 m-2">
+		<button class="rounded bg-gray-400 p-4 hover:bg-gray-600" on:click={() => setSchedule()}
+			>Generate Schedule</button
+		>
+	</div>
+	<div class="w-1/2 m-2">
+		{JSON.stringify(schedule) || 'Schedule..'}
+	</div>
+</div>
