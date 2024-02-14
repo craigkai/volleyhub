1f5a20193a71f7f6b1a0380ed2173fd96b761aec
Craig Kaiser <craig@kaiserengineering.io>
2024-02-14 14:25:11 -0500

    start working on bracket logic

--- src/components/Bracket.svelte
+++ src/components/Bracket.svelte
@@ -2,358 +2,81 @@
 	import { Event } from '$lib/event';
 	import { Matches } from '$lib/matches';
 	import type { Teams } from '$lib/teams';
-	import { SingleEliminationBracket } from '$lib/bracket';
-	import { error } from '$lib/toast';
-	import type { HttpError } from '@sveltejs/kit';
 
 	export let tournament: Event;
 	export let matches: Matches;
 	export let teams: Teams;
 
 	const teamNames = teams.teams.map((team) => team.name);
-	let bracketStructure = [];
-
-	try {
-		const bracket = new SingleEliminationBracket(teamNames);
-		bracketStructure = bracket.getBracket(); // Assign the bracket structure
-	} catch (err) {
-		error((err as HttpError).toString());
-	}
 
 	matches.loadBracketMatches();
+
+	const numRounds = teamNames.length / 2 + (teamNames.length % 2);
+
+	// TODO: Create a listener for when bracket macthes are updated, and auto create the next match in the bracket.
+	// TODO: Allow bracket matches to be edited.
 </script>
 
-<!--
-	Need to iterate over each round (should we make rounds text instead of ints? So we can have
-	a round like `semi-finals` instead of `3`?). Then iterate over each match in the round and generate our table.
--->
-
 <div class="container">
-	<h1>{tournament.name}</h1>
 	<div class="tournament-bracket tournament-bracket--rounded">
-		<div class="tournament-bracket__round tournament-bracket__round--quarterfinals">
-			<h3 class="tournament-bracket__round-title">Quarterfinals</h3>
-			<ul class="tournament-bracket__list">
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<!-- <caption class="tournament-bracket__caption">
-								<time datetime="1998-02-18">18 February 1998</time>
-							</caption> -->
-							<thead class="sr-only">
-								<tr>
-									<th>Team</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Canada">CAN</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-ca" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">4</span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Kazakhstan">KAZ</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-kz" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">1</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Czech Republic">CZE</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-cz" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">4</span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Unitede states of America"
-											>USA</abbr
-										>
-										<span class="tournament-bracket__flag flag-icon flag-icon-us" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">1</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Finland">FIN</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-fi" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">2</span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Sweden">SVE</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-se" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">1</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Russia">RUS</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-ru" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">4</span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Belarus">BEL</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-by" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">1</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<div class="tournament-bracket__round tournament-bracket__round--semifinals">
-			<h3 class="tournament-bracket__round-title">Semifinals</h3>
-			<ul class="tournament-bracket__list">
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Canada">CAN</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-ca" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">1</span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Czech Republic">CZE</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-cz" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">2</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Finland">FIN</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-fi" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">4</span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Russia">RUS</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-ru" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">7</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<div class="tournament-bracket__round tournament-bracket__round--bronze">
-			<h3 class="tournament-bracket__round-title">Bronze medal game</h3>
-			<ul class="tournament-bracket__list">
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Finland">FIN</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-fi" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">3</span>
-										<span
-											class="tournament-bracket__medal tournament-bracket__medal--bronze fa fa-trophy"
-											aria-label="Bronze medal"
-										></span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Canada">CAN</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-ca" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">2</span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<div class="tournament-bracket__round tournament-bracket__round--gold">
-			<h3 class="tournament-bracket__round-title">Gold medal game</h3>
-			<ul class="tournament-bracket__list">
-				<li class="tournament-bracket__item">
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div class="tournament-bracket__match" tabindex="0">
-						<table class="tournament-bracket__table">
-							<caption class="tournament-bracket__caption">
-								<time datetime="1998-02-22">22 February 1998</time>
-							</caption>
-							<thead class="sr-only">
-								<tr>
-									<th>Country</th>
-									<th>Score</th>
-								</tr>
-							</thead>
-							<tbody class="tournament-bracket__content">
-								<tr class="tournament-bracket__team tournament-bracket__team--winner">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Czech Republic">CZE</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-cz" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">1</span>
-										<span
-											class="tournament-bracket__medal tournament-bracket__medal--gold fa fa-trophy"
-											aria-label="Gold medal"
-										></span>
-									</td>
-								</tr>
-								<tr class="tournament-bracket__team">
-									<td class="tournament-bracket__country">
-										<abbr class="tournament-bracket__code" title="Russia">RUS</abbr>
-										<span class="tournament-bracket__flag flag-icon flag-icon-ru" aria-label="Flag"
-										></span>
-									</td>
-									<td class="tournament-bracket__score">
-										<span class="tournament-bracket__number">0</span>
-										<span
-											class="tournament-bracket__medal tournament-bracket__medal--silver fa fa-trophy"
-											aria-label="Silver medal"
-										></span>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</li>
-			</ul>
-		</div>
+		{#each Array(numRounds) as _, i}
+			{@const matchesInRound = matches?.bracketMatches?.filter((match) => match.round === i) || []}
+			<div class="tournament-bracket__round tournament-bracket__round--quarterfinals">
+				<h3 class="tournament-bracket__round-title">Round {i + 1}</h3>
+				<ul class="tournament-bracket__list">
+					{#if matchesInRound.length != 0}
+						{#each matchesInRound as match}
+							{@const team1Win =
+								match.team1_score && match.team2_score
+									? match.team1_score > match.team2_score
+									: false}
+							{@const team2Win = !team1Win && match.team1_score && match.team2_score}
+							<li class="tournament-bracket__item">
+								<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+								<div class="tournament-bracket__match" tabindex="0">
+									<table class="tournament-bracket__table">
+										<thead class="sr-only">
+											<tr>
+												<th>Team</th>
+												<th>Score</th>
+											</tr>
+										</thead>
+										<tbody class="tournament-bracket__content">
+											<tr
+												class:tournament-bracket__team--winner={team1Win}
+												class="tournament-bracket__team"
+											>
+												<td class="tournament-bracket__country">
+													<abbr class="tournament-bracket__code" title="team1"
+														>{match.matches_team1_fkey.name}</abbr
+													>
+												</td>
+												<td class="tournament-bracket__score">
+													<span class="tournament-bracket__number">{match?.team1_score}</span>
+												</td>
+											</tr>
+											<tr
+												class:tournament-bracket__team--winner={team2Win}
+												class="tournament-bracket__team"
+											>
+												<td class="tournament-bracket__country">
+													<abbr class="tournament-bracket__code" title="team1"
+														>{match.matches_team2_fkey.name}</abbr
+													>
+												</td>
+												<td class="tournament-bracket__score">
+													<span class="tournament-bracket__number">{match?.team2_score}</span>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</li>
+						{/each}
+					{/if}
+				</ul>
+			</div>
+		{/each}
 	</div>
 </div>
 
@@ -839,7 +562,7 @@
 		border-color: spin(shade(#f5f5f5, 10%), -10);
 
 		.tournament-bracket__team--winner & {
-			background-color: #fff176;
+			background-color: rgb(155, 243, 155);
 			border-color: spin(shade(#fff176, 2%), -10);
 		}
 	}
