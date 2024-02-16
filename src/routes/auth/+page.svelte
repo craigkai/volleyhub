7d60a43d3b8dca6bad8f9436fb8dbba32c38881a
Craig Kaiser <craig@kaiserengineering.io>
2024-02-16 07:36:03 -0500

    test removing old svelte auth lib

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,9 +1,63 @@
 <script lang="ts">
 	import type { PageData } from '$types';
-	import { Auth } from '@supabase/auth-ui-svelte';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
+	import { goto } from '$app/navigation';
 
 	export let data: PageData;
+	let { supabase } = data;
+	$: ({ supabase } = data);
+
+	let email: string;
+	let password: string;
+	let errorString = '',
+		message = '';
+
+	const handleSignUp = async () => {
+		const { error: err } = await supabase.auth.signUp({
+			email,
+			password,
+			options: {
+				emailRedirectTo: `${location.origin}/auth/callback`
+			}
+		});
+		if (err) errorString = err.message;
+		else message = "We've sent you an email to confirm your account";
+	};
+
+	const handleSignInOtp = async () => {
+		const { error: err } = await supabase.auth.signInWithOtp({
+			email: email,
+			options: {
+				emailRedirectTo: `${data.url}/auth/callback`
+			}
+		});
+		if (err) errorString = err.message;
+		else message = 'Check your email for the magic link.';
+	};
+
+	const handleForgotPassword = async () => {
+		const { error: err } = await supabase.auth.resetPasswordForEmail(email, {
+			redirectTo: `${data.url}/auth/recovery`
+		});
+		if (err) errorString = err.message;
+		else message = 'Check your email for the password reset link.';
+	};
+
+	const handleSignIn = async () => {
+		const { error: err } = await supabase.auth.signInWithPassword({
+			email,
+			password
+		});
+		if (err) errorString = err.message;
+		else goto('/protected-routes/dashboard');
+	};
+
+	const handleSignOut = async () => {
+		const { error: err } = await supabase.auth.signOut();
+		if (err) errorString = err.message;
+		else goto('/');
+	};
+
+	let method = 'magic';
 </script>
 
 <svelte:head>
@@ -12,12 +66,42 @@
 
 <div class="row flex-center flex justify-center">
 	<div class="col-6 form-widget">
-		<Auth
-			supabaseClient={data.supabase}
-			view="magic_link"
-			redirectTo={`${data.url}/auth/recovery`}
-			showLinks={true}
-			appearance={{ theme: ThemeSupa, className: { input: ['text-black', 'dark:text-white'] } }}
-		/>
+		{#if method == 'magic'}
+			<input name="email" bind:value={email} />
+			<button on:click={handleSignInOtp}>Send a magic link</button>
+		{:else if method == 'password'}
+			<input name="email" bind:value={email} />
+			<input type="password" name="password" bind:value={password} />
+			<button on:click={handleSignIn}>Sign in</button>
+			<button on:click={() => (method = 'recovery')}>Forgot your password?</button>
+		{:else if method == 'signup'}
+			<form on:submit={handleSignUp}>
+				<input name="email" bind:value={email} />
+				<input type="password" name="password" bind:value={password} />
+				<button>Sign up</button>
+			</form>
+		{:else if method == 'recovery'}
+			<input name="email" bind:value={email} />
+			<button on:click={handleForgotPassword}>Send reset password instructions</button>
+		{/if}
+
+		<button on:click={() => (method = 'password')}>Already have an account? Sign in</button>
+		<button on:click={handleSignOut}>Sign out</button>
 	</div>
 </div>
+
+{#if message}
+	<span>{message}</span>
+{/if}
+
+{#if errorString}
+	<span class="danger">
+		{errorString}
+	</span>
+{/if}
+
+<style>
+	.danger {
+		color: rgba(245, 101, 101);
+	}
+</style>
