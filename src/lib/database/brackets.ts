c7517482056800549d5395953e3386388f403af3
Craig Kaiser <craig@kaiserengineering.io>
2024-02-16 23:56:37 -0500

    format

--- src/lib/database/brackets.ts
+++ src/lib/database/brackets.ts
@@ -6,65 +6,65 @@ import { bracketsRowSchema } from '../../types/schemas';
 const BracketsRowSchemaArray = z.array(bracketsRowSchema);
 
 type Filter = {
-    column: string;
-    operator: string;
-    value: string;
+	column: string;
+	operator: string;
+	value: string;
 };
 
 export class BracketsSupabaseDatabaseService extends SupabaseDatabaseService {
-    /**
-     * Load all matches associated with a specific event from the database.
-     * @param {string} event_id - The ID of the event whose matches to load.
-     * @returns {Promise<MatchRow[]>} - Returns a promise that resolves to an array of the loaded matches.
-     * @throws {Error} - Throws an error if there's an issue loading the matches.
-     */
-    async load(event_id: number, filter?: Filter): Promise<BracketRow[] | null> {
-        const query = this.supabaseClient
-            .from('brackets')
-            .select('*, brackets_team1_fkey(name), brackets_team2_fkey(name)')
-            .eq('event_id', event_id);
+	/**
+	 * Load all matches associated with a specific event from the database.
+	 * @param {string} event_id - The ID of the event whose matches to load.
+	 * @returns {Promise<MatchRow[]>} - Returns a promise that resolves to an array of the loaded matches.
+	 * @throws {Error} - Throws an error if there's an issue loading the matches.
+	 */
+	async load(event_id: number, filter?: Filter): Promise<BracketRow[] | null> {
+		const query = this.supabaseClient
+			.from('brackets')
+			.select('*, brackets_team1_fkey(name), brackets_team2_fkey(name)')
+			.eq('event_id', event_id);
 
-        if (filter) {
-            query.filter(filter.column, filter.operator, filter.value);
-        }
+		if (filter) {
+			query.filter(filter.column, filter.operator, filter.value);
+		}
 
-        // Load the matches from the 'matches' table
-        const res: PostgrestResponse<BracketRow[]> = await query;
+		// Load the matches from the 'matches' table
+		const res: PostgrestResponse<BracketRow[]> = await query;
 
-        this.validateAndHandleErrors(res, BracketsRowSchemaArray);
+		this.validateAndHandleErrors(res, BracketsRowSchemaArray);
 
-        // Return the loaded matches
-        return res.data;
-    }
+		// Return the loaded matches
+		return res.data;
+	}
 
-    async deleteBracketMatchesByEvent(event_id: number): Promise<void> {
-        const response = await this.supabaseClient.from('matches').delete().eq('event_id', event_id);
-        this.handleDatabaseError(response);
-    }
+	async deleteBracketMatchesByEvent(event_id: number): Promise<void> {
+		const response = await this.supabaseClient.from('matches').delete().eq('event_id', event_id);
+		this.handleDatabaseError(response);
+	}
 
-    async insertBracketMatches(matches: UserMatch[]): Promise<MatchRow[]> {
-        const res = await this.supabaseClient
-            .from('brackets')
-            .insert(matches)
-            .select('*, brackets_team1_fkey(name), brackets_team2_fkey(name)');
+	async insertBracketMatches(matches: UserMatch[]): Promise<MatchRow[]> {
+		const res = await this.supabaseClient
+			.from('brackets')
+			.insert(matches)
+			.select('*, brackets_team1_fkey(name), brackets_team2_fkey(name)');
 
-        this.validateAndHandleErrors(res, BracketsRowSchemaArray);
+		this.validateAndHandleErrors(res, BracketsRowSchemaArray);
 
-        return res.data ?? [];
-    }
+		return res.data ?? [];
+	}
 
-    async updateMatch(match: MatchRow): Promise<MatchRow | null> {
-        const parsedMatch = bracketsRowSchema.parse(match);
+	async updateMatch(match: MatchRow): Promise<MatchRow | null> {
+		const parsedMatch = bracketsRowSchema.parse(match);
 
-        const res = await this.supabaseClient
-            .from('brackets')
-            .update(parsedMatch)
-            .eq('id', match.id)
-            .select('*, brackets_team1_fkey(name), brackets_team2_fkey(name)')
-            .single();
+		const res = await this.supabaseClient
+			.from('brackets')
+			.update(parsedMatch)
+			.eq('id', match.id)
+			.select('*, brackets_team1_fkey(name), brackets_team2_fkey(name)')
+			.single();
 
-        this.validateAndHandleErrors(res, bracketsRowSchema);
+		this.validateAndHandleErrors(res, bracketsRowSchema);
 
-        return res.data;
-    }
+		return res.data;
+	}
 }
