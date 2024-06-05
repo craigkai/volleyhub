0605ded1a61d42bf6354ba36defaf853b6eef28e
Craig Kaiser <craig@kaiserengineering.io>
2024-06-05 10:02:20 -0400

    fix loader on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { EventSupabaseDatabaseService } from '$lib/database/event';
-	import { Spinner } from 'flowbite-svelte';
+	import { Loader } from 'lucide-svelte';
 	import EventsCards from '$components/eventsCards.svelte';
 
 	export let data: PageData;
@@ -11,7 +11,7 @@
 
 {#await eventsPromise}
 	<div class="h-screen flex flex-col items-center place-content-center">
-		<Spinner />
+		<Loader class="animate-spin" />
 	</div>
 {:then events}
 	<div class="flex flex-col items-center">
