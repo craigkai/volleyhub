31441ebe3a82d5d0cdc40dace584fa4ebcbbdfd3
Craig Kaiser <craig@kaiserengineering.io>
2024-06-16 11:11:11 -0400

    bump

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -89,65 +89,99 @@
 	<title>User Auth</title>
 </svelte:head>
 
-<div class="flex justify-center items-center min-h-screen">
+<div class="flex justify-center items-center min-h-screen bg-gray-100">
 	<div class="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md dark:bg-gray-800">
+		<h2 class="text-2xl font-bold text-center text-gray-700 dark:text-gray-200">
+			User Authentication
+		</h2>
 		{#if method === 'magic'}
-			<Label for="email" class="text-gray-400">Email Address</Label>
-			<Input name="email" class="mb-4 bg-gray-200 border border-2 p-4" bind:value={email} />
-			<Button class="w-full p-3 text-white bg-blue-400 hover:bg-blue-600" on:click={handleSignInOtp}
-				>Send a magic link</Button
-			>
+			<div class="space-y-4">
+				<Label for="email" class="text-gray-700 dark:text-gray-400">Email Address</Label>
+				<Input
+					name="email"
+					class="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+					bind:value={email}
+				/>
+				<Button
+					class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+					on:click={handleSignInOtp}>Send Magic Link</Button
+				>
+			</div>
 		{:else if method === 'password'}
-			<Label for="email" class="text-gray-400">Email Address</Label>
-			<Input name="email" id="email" class="mb-4 p-4 bg-gray-200" bind:value={email} />
-			<Label for="password" class="text-gray-400">Password</Label>
-			<Input
-				type="password"
-				id="password"
-				class="mb-4"
-				bind:value={password}
-				placeholder="•••••••••"
-				required
-			/>
-			<Button class="w-full text-white bg-blue-400 hover:bg-blue-600 mb-2" on:click={handleSignIn}
-				>Sign in</Button
-			>
-			<Button
-				class="w-full text-white bg-blue-400 hover:bg-blue-600"
-				on:click={() => (method = 'recovery')}>Forgot your password?</Button
-			>
-		{:else if method === 'signup'}
-			<form on:submit|preventDefault={handleSignUp}>
-				<Label for="email" class="text-gray-400">Email Address</Label>
-				<Input name="email" id="email" class="mb-4" bind:value={email} />
-				<Label for="password" class="text-gray-400">Password</Label>
+			<div class="space-y-4">
+				<Label for="email" class="text-gray-700 dark:text-gray-400">Email Address</Label>
+				<Input
+					name="email"
+					id="email"
+					class="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+					bind:value={email}
+				/>
+				<Label for="password" class="text-gray-700 dark:text-gray-400">Password</Label>
 				<Input
 					type="password"
 					id="password"
-					class="mb-4"
+					class="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
 					bind:value={password}
 					placeholder="•••••••••"
 					required
 				/>
-				<Button type="submit" class="w-full text-white bg-blue-400 hover:bg-blue-600"
-					>Sign up</Button
+				<Button
+					class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+					on:click={handleSignIn}>Sign In</Button
+				>
+				<Button
+					class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+					on:click={() => (method = 'recovery')}>Forgot your password?</Button
+				>
+			</div>
+		{:else if method === 'signup'}
+			<form class="space-y-4" on:submit|preventDefault={handleSignUp}>
+				<Label for="email" class="text-gray-700 dark:text-gray-400">Email Address</Label>
+				<Input
+					name="email"
+					id="email"
+					class="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+					bind:value={email}
+				/>
+				<Label for="password" class="text-gray-700 dark:text-gray-400">Password</Label>
+				<Input
+					type="password"
+					id="password"
+					class="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+					bind:value={password}
+					placeholder="•••••••••"
+					required
+				/>
+				<Button
+					type="submit"
+					class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+					>Sign Up</Button
 				>
 			</form>
 		{:else if method === 'recovery'}
-			<Label for="email" class="text-gray-400">Email Address</Label>
-			<Input name="email" class="mb-4" bind:value={email} />
-			<Button
-				class="w-full text-white bg-blue-400 hover:bg-blue-600"
-				on:click={handleForgotPassword}>Send reset password instructions</Button
-			>
+			<div class="space-y-4">
+				<Label for="email" class="text-gray-700 dark:text-gray-400">Email Address</Label>
+				<Input
+					name="email"
+					class="w-full px-4 py-2 text-gray-700 bg-gray-200 rounded-md border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+					bind:value={email}
+				/>
+				<Button
+					class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+					on:click={handleForgotPassword}>Send Reset Password Instructions</Button
+				>
+			</div>
 		{/if}
-		<Button
-			class="w-full text-white bg-blue-400 hover:bg-blue-600 mt-4"
-			on:click={() => (method = 'password')}>Already have an account? Sign in</Button
-		>
-		<Button class="w-full text-white bg-blue-400 hover:bg-blue-600 mt-4" on:click={handleSignOut}
-			>Sign out</Button
-		>
+		<div class="space-y-4">
+			<Button
+				class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+				on:click={() => (method = 'password')}>Already have an account? Sign In</Button
+			>
+			<Button
+				class="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+				on:click={handleSignOut}>Sign Out</Button
+			>
+		</div>
 		{#if message}
 			<div class="text-center text-green-500 mt-4">{message}</div>
 		{/if}
