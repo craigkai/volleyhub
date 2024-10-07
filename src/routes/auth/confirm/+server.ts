80acc4f6abe1be6c35394a9c61ff2900686ba16d
Craig Kaiser <craig@bestpractical.com>
2024-10-07 08:44:00 -0400

    fix auth redirect

--- src/routes/auth/confirm/+server.ts
+++ src/routes/auth/confirm/+server.ts
@@ -7,7 +7,7 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
 	const token_hash = url.searchParams.get('token_hash');
 	const type = url.searchParams.get('type') as EmailOtpType | null;
-	const next = url.searchParams.get('next') ?? '/account';
+	const next = url.searchParams.get('next') ?? '/protected-routes/dashboard';
 
 	/**
 	 * Clean up the redirect URL by deleting the Auth flow parameters.
