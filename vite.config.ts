130611de3632fe0c5c6dd4b9ff4435113e968d34
Craig Kaiser <craig@bestpractical.com>
2024-10-14 20:53:16 -0400

    remove custom plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		myErrorFilterPlugin(),
 		VitePWA({
 			registerType: 'autoUpdate',
 			devOptions: {
@@ -20,31 +19,3 @@ export default defineConfig({
 		sourcemap: true
 	}
 });
-
-export function myErrorFilterPlugin(): Plugin {
-	return {
-		name: 'error-filter-plugin',
-		configureServer(server) {
-			const filterMessage =
-				'Using the user object as returned from supabase.auth.getSession() or from some supabase.auth.onAuthStateChange() events could be insecure! This value comes directly from the storage medium (usually cookies on the server) and many not be authentic. Use supabase.auth.getUser() instead which authenticates the data by contacting the Supabase Auth server.';
-
-			// Intercept console.warn
-			const originalWarn = console.warn;
-			console.warn = (...args: any[]) => {
-				if (args[0] && typeof args[0] === 'string' && args[0].includes(filterMessage)) {
-					return;
-				}
-				originalWarn(...args);
-			};
-
-			// Intercept console.log
-			const originalLog = console.log;
-			console.log = (...args: any[]) => {
-				if (args[0] && typeof args[0] === 'string' && args[0].includes(filterMessage)) {
-					return;
-				}
-				originalLog(...args);
-			};
-		}
-	};
-}
