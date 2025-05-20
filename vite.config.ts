649592a1bf5955926cef956d8189ed0dc3ce15df
Craig Kaiser <craig@bestpractical.com>
2025-05-20 12:56:53 -0400

    Add missing node modules

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sveltekit()
-	],
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: ['ws']
+	},
+	resolve: {
+		alias: {
+			ws: '/dev/null'
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	build: {
-		sourcemap: true
+		sourcemap: true,
+		rollupOptions: {
+			external: ['ws']
+		}
 	}
 });
