0bc2f54f4655c547b5d7c95c5f8c051ebf3ec7a4
Craig Kaiser <craig@bestpractical.com>
2025-05-20 13:00:12 -0400

    revert

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,27 +1,20 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import type { LayoutLoad } from './$types';
+import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr';
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
-	let supabase;
-
-	if (typeof window !== 'undefined') {
-		const { createBrowserClient } = await import('@supabase/ssr');
-		supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-			global: { fetch }
-		});
-	} else {
-		const { createServerClient } = await import('@supabase/ssr');
-		supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-			global: { fetch },
-			cookies: {
-				getAll() {
-					return data.cookies;
+	const supabase = isBrowser()
+		? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, { global: { fetch } })
+		: createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: { fetch },
+				cookies: {
+					getAll() {
+						return data.cookies;
+					}
 				}
-			}
-		});
-	}
+			});
 
 	const sessionRes = await supabase.auth.getSession();
 	const session = sessionRes.data.session;
--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: ['ws']
-	},
-	resolve: {
-		alias: {
-			ws: '/dev/null'
-		}
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	build: {
-		sourcemap: true,
-		rollupOptions: {
-			external: ['ws']
-		}
+		sourcemap: true
 	}
 });
