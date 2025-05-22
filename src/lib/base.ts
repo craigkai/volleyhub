4653c8ada8fc790af1118735fccf53651a28d038
Craig Kaiser <craig@bestpractical.com>
2025-05-22 19:19:06 -0400

    Remove double logging

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -2,7 +2,6 @@ import type { NumericRange } from '@sveltejs/kit';
 
 export class Base {
 	handleError(statusCode: NumericRange<400, 599>, message: string) {
-		console.error(message);
 		throw new Error(message);
 	}
 }
