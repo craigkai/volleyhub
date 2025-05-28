f6987d710d546b38ba4c925146d38790632c0ee3
Craig Kaiser <craig@bestpractical.com>
2025-05-28 14:29:59 -0400

    Labels

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -224,7 +224,7 @@
 											{...props}
 											class="mt-1.5 min-w-[8rem] border-gray-300 text-sm focus:border-emerald-500 focus:ring-emerald-500 sm:text-base dark:border-gray-600"
 										>
-											{$formData.refs}
+											{$formData.refs.capitalize()}
 										</SelectTrigger>
 										<SelectContent class="bg-white dark:bg-gray-800">
 											<SelectItem value="teams" label="Teams" />
