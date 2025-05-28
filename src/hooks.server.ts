d64c16e50e27d5e3f4f89b030dca2f7b85875758
Craig Kaiser <craig@bestpractical.com>
2025-05-28 14:31:27 -0400

    Remove testing code

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -62,7 +62,7 @@ export const supabase: Handle = async ({ event, resolve }) => {
 		return { session, user };
 	};
 
-	if (0&&import.meta.env.DEV) {
+	if (import.meta.env.DEV) {
 		const {
 			data: { session }
 		} = await event.locals.supabase.auth.getSession();
