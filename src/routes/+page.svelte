890e02f682522250194445e0f148f817a053443c
Craig Kaiser <craig@bestpractical.com>
2025-06-05 22:07:38 -0400

    Remove extra min-h-screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,6 @@
 	<title>Dashboard - Volleyhub</title>
 </svelte:head>
 
-<div class="min-h-screen bg-gray-50 dark:bg-gray-900">
-	<main class="mx-auto max-w-7xl px-3 py-6 sm:px-6 lg:px-8">
-		<UpcomingEvents {data} />
-	</main>
-</div>
+<main class="mx-auto max-w-7xl px-3 py-6 sm:px-6 lg:px-8">
+	<UpcomingEvents {data} />
+</main>
