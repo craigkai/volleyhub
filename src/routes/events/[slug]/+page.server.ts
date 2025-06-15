efe4d88dd611ab2ac9f1c3e56a307d0022eb1e0c
Craig Kaiser <craig@bestpractical.com>
2025-06-15 17:30:11 -0400

    Do not assume ZodError in action endpoint

--- src/routes/events/[slug]/+page.server.ts
+++ src/routes/events/[slug]/+page.server.ts
@@ -1,19 +1,19 @@
 import { superValidate } from 'sveltekit-superforms';
 import { formSchema as settingsSchema } from '$schemas/settingsSchema';
 import { formSchema as teamsSchema } from '$schemas/teamsSchema';
-import { eventsInsertSchema, eventsUpdateSchema, teamsInsertSchema } from '$schemas/supabase';
+import { eventsUpdateSchema } from '$schemas/supabase';
 import { zod } from 'sveltekit-superforms/adapters';
 import type { PageServerLoad, Actions } from './$types';
 import { fail, redirect } from '@sveltejs/kit';
 import { Event } from '$lib/event.svelte';
 import { EventSupabaseDatabaseService } from '$lib/database/event';
-import type { ZodError } from 'zod';
+import { ZodError } from 'zod';
 import { fromZodError } from 'zod-validation-error';
 import { TeamsSupabaseDatabaseService } from '$lib/database/teams';
 import { Teams } from '$lib/teams.svelte';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const { data, error } = await locals.supabase.auth.getUser();
+	const { data } = await locals.supabase.auth.getUser();
 
 	return {
 		user: data?.user
@@ -23,51 +23,39 @@ export const load: PageServerLoad = async ({ locals }) => {
 export const actions: Actions = {
 	updateEvent: async (event) => {
 		const form = await superValidate(event, zod(settingsSchema));
-		if (!form.valid) {
-			return fail(400, {
-				form
-			});
-		}
+		if (!form.valid) return fail(400, { form });
 
 		const eventId = Number(event.params.slug);
+		if (!event.params.slug) throw new Error('Slug is undefined');
 
-		if (!event.params.slug) {
-			throw new Error('Slug is undefined');
-		}
 		const eventSupabaseDatabaseService = new EventSupabaseDatabaseService(event.locals.supabase);
 		const tournament = new Event(eventSupabaseDatabaseService);
 
 		try {
 			const updatedTournament = await tournament.update(eventId, form.data);
-
-			// Parse the updated tournament data
 			const parsedData = eventsUpdateSchema.parse(updatedTournament);
-
-			// Update the form data with the parsed and updated values
 			form.data = parsedData as typeof form.data;
-			return {
-				form
-			};
+			return { form };
 		} catch (error) {
 			form.valid = false;
 			console.error(error);
 
-			const validationError = fromZodError(error as ZodError<typeof eventsUpdateSchema>);
-			form.message = validationError.message;
+			if (error instanceof ZodError) {
+				const validationError = fromZodError(error);
+				form.message = validationError.message;
+			} else if (error instanceof Error) {
+				form.message = error.message;
+			} else {
+				form.message = 'An unexpected error occurred.';
+			}
 
-			return fail(400, {
-				form
-			});
+			return fail(400, { form });
 		}
 	},
 
 	createEvent: async (event) => {
 		const form = await superValidate(event, zod(settingsSchema));
-		if (!form.valid) {
-			return fail(400, {
-				form
-			});
-		}
+		if (!form.valid) return fail(400, { form });
 
 		const eventSupabaseDatabaseService = new EventSupabaseDatabaseService(event.locals.supabase);
 		const tournament = new Event(eventSupabaseDatabaseService);
@@ -76,41 +64,42 @@ export const actions: Actions = {
 
 		try {
 			const res = await tournament.create(form.data);
-			if (res.id) {
+			if (res?.id) {
 				newId = res.id;
 			} else {
 				form.valid = false;
 				form.message = 'Failed to create event';
-
-				return fail(400, {
-					form
-				});
+				return fail(400, { form });
 			}
 		} catch (error) {
 			form.valid = false;
 			console.error(error);
 
-			const validationError = fromZodError(error as ZodError<typeof eventsInsertSchema>);
-			form.message = validationError.message;
+			if (error instanceof ZodError) {
+				const validationError = fromZodError(error);
+				form.message = validationError.message;
+			} else if (error instanceof Error) {
+				form.message = error.message;
+			} else {
+				form.message = 'An unexpected error occurred.';
+			}
 
-			return fail(400, {
-				form
-			});
+			return fail(400, { form });
 		}
+
 		redirect(303, `/events/${newId}`);
 	},
 
 	deleteEvent: async (event) => {
 		const eventId = Number(event.params.slug);
-
 		const eventSupabaseDatabaseService = new EventSupabaseDatabaseService(event.locals.supabase);
 		const tournament = new Event(eventSupabaseDatabaseService);
+
 		try {
 			await tournament.load(eventId);
-
-			// TODO: How do we handle delete failure?
 			await tournament.delete();
 		} catch (error) {
+			console.error(error);
 			return fail(400, {});
 		}
 
@@ -119,14 +108,9 @@ export const actions: Actions = {
 
 	createTeam: async (event) => {
 		const form = await superValidate(event, zod(teamsSchema));
-		if (!form.valid) {
-			return fail(400, {
-				form
-			});
-		}
+		if (!form.valid) return fail(400, { form });
 
 		const eventId = Number(event.params.slug);
-
 		const teamsSupabaseDatabaseService = new TeamsSupabaseDatabaseService(event.locals.supabase);
 		const teams = new Teams(teamsSupabaseDatabaseService);
 
@@ -139,13 +123,18 @@ export const actions: Actions = {
 			return { form };
 		} catch (error) {
 			form.valid = false;
+			console.error(error);
 
-			const validationError = fromZodError(error as ZodError<typeof teamsInsertSchema>);
-			form.message = validationError.message;
+			if (error instanceof ZodError) {
+				const validationError = fromZodError(error);
+				form.message = validationError.message;
+			} else if (error instanceof Error) {
+				form.message = error.message;
+			} else {
+				form.message = 'An unexpected error occurred.';
+			}
 
-			return fail(400, {
-				form
-			});
+			return fail(400, { form });
 		}
 	}
 };
